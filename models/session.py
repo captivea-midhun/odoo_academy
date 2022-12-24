5f006720f1cd70d90e89a17f456f4954c98d5536
root <root@Midhun.localdomain>
2022-12-24 17:52:47 -0500

    Testing

--- models/session.py
+++ models/session.py
@@ -3,6 +3,7 @@
 # Description: Holds the class representing the model
 
 from odoo import fields, api, models
+from datetime import timedelta
 
 class Session(models.Model):
     _name = 'academy.session'
@@ -23,3 +24,27 @@ class Session(models.Model):
 
     # A student can attend many sessions and a session can have multiple students
     student_ids = fields.Many2many(comodel_name='res.partner', string='Students')
+
+    start_date = fields.Date(string='Start Date', default=fields.Date.today)
+
+    duration = fields.Integer(string='Session days', default=1)
+
+    end_date = fields.Date(string='End date', compute='_compute_end_date', inverse='_inverse_end_date', stored=True)
+
+    @api.depends('start_date', 'duration')
+    def _compute_end_date(self):
+        """Compute function to update end_date. This will run if either start_date or duration is updated."""
+        for rec in self:
+            if not rec.start_date and rec.duration:
+                rec.end_date = rec.start_date
+            else:
+                duration = timedelta(days=rec.duration)
+                rec.end_date = rec.start_date + duration
+    
+    def _inverse_end_date(self):
+        """xxx"""
+        for rec in self:
+            if rec.start_date and rec.end_date:
+                rec.duration = (rec.end_date - rec.start_date).days + 1      
+            else:
+                continue 
